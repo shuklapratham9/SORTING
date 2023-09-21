33cebd4471a3cc9c333e32fa88938433074a4d0d
shuklapratham9 <130244305+shuklapratham9@users.noreply.github.com>
2023-09-22 00:17:57 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,28 @@
+package SORTING;
+
+import java.util.Scanner;
+
+public class Bubblesort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.println(a[k]);
+
+        }
+        sc.close();
+    }
+}
